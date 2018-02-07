0a6b14ba9e8652175ac9a95de483815d51e8fe72
Aulas <Aulas>
2018-02-06 23:54:04 -0200

    Aula 9

---
+++ Aulas/Aula 9/Aula 9.R
@@ -0,0 +1,11 @@
+#Aula 9 - Objeto ts
+
+ts(30:50)
+
+ts(1:23, start=2010)
+
+ts(1:24, start=2010, frequency = 12)
+
+ts(1:24, start=2010, end=c(2011,6) , frequency = 12)
+
+ts(1:24, start=c(2010,5), end=c(2011,6) , frequency = 12)
